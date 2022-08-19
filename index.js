77dac9b5c655fa3480b3bbf7b3a78acf68e6dda5
Sealajoke <102927988+Sealajoke@users.noreply.github.com>
2022-08-19 09:07:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ function introduction(name) {
 }
 
 introduction(Ali)
+function introduction(name) {
+    console.log(`Hi, my name is ${name}`)
+}
+
+introduction("Ali")
+
+function introductionWithLanguage(name, language){
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`)
+
+}
+
+introductionWithLanguage("Ali", "Javascript")
+
+function introductionWithLanguageOptional(name, language="Javascript"){
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`)
+}
+
+introductionWithLanguageOptional("Ali", )
